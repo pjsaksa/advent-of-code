2eb0a6a6846e7c5c738b46bfdfefc02965ffcb61
Pauli Saksa <pjsaksa@gmail.com>
2019-12-03 00:30:30 +0200

    added day 2 puzzle

---
+++ 2019/02b.cc
@@ -0,0 +1,43 @@
+#include "02-common.hh"
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+enum { Result = 19690720 };
+
+int main()
+{
+    try
+    {
+        vector<int> saved;
+
+        readCodes(saved);
+
+        for (int noun = 0;
+             noun < 100;
+             ++noun)
+        {
+            for (int verb = 0;
+                 verb < 100;
+                 ++verb)
+            {
+                vector<int> codes = saved;
+
+                codes.at(1) = noun;
+                codes.at(2) = verb;
+
+                runCodes(codes);
+
+                if (codes.at(0) == Result)
+                {
+                    cout << "With noun " << noun << " and verb " << verb << " the value at position 0 is " << Result << endl
+                         << "Answer: " << (100 * noun + verb) << endl;
+                }
+            }
+        }
+    }
+    catch (exception& e) {
+        cerr << "exception: " << e.what() << endl;
+    }
+}
