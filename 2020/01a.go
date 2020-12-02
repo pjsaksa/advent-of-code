7155d0f04bb93146a90c4816ccc610aa20435052
Pauli Saksa <pjsaksa@gmail.com>
2020-12-02 23:23:03 +0200

    added day 1 second puzzle, and build/test scripts

--- 2020/01a.go
+++ 2020/01a.go
@@ -2,51 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"sort"
 )
 
-type Entries []int
-
-func (entries *Entries) readInput(fileName string) {
-	const (
-		ReadChunk = 50
-	)
-
-	//
-
-	file,err := os.Open(fileName)
-
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	//
-
-	var chunk [ReadChunk]int
-
-	for {
-		for i := 0; i < ReadChunk; i++ {
-			_,err := fmt.Fscanln(file, &chunk[i])
-
-			if err == io.EOF {
-				*entries = append(*entries, chunk[0:i]...)
-				return
-			} else if err != nil {
-				panic(err)
-			}
-		}
-
-		*entries = append(*entries, chunk[0:ReadChunk]...)
-	}
-}
-
 func main() {
 	var entries Entries
 
-	entries.readInput("input-01.txt")
+	entries.ReadInput("input-01.txt")
 
 	if len(entries) == 0 {
 		panic("empty input")
@@ -74,6 +37,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Match: %v + %v = %v\n", entries[begin], entries[end], entries[begin] + entries[end])
-	fmt.Printf("Match: %v * %v = %v\n", entries[begin], entries[end], entries[begin] * entries[end])
+	fmt.Println("Match:")
+	fmt.Println("-", entries[begin], "+", entries[end], "=", entries[begin] + entries[end])
+	fmt.Println("-", entries[begin], "+", entries[end], "=", entries[begin] * entries[end])
 }
